a48673b4e98adbc318f822382398d8bae94e8160
Scott <scott@brendas-imac.local>
2018-06-14 14:12:19 -0700

    Initial Commit -Shows a gallery of photos

--- PhotoTagger/AppDelegate.swift
+++ PhotoTagger/AppDelegate.swift
@@ -7,40 +7,68 @@
 //
 
 import UIKit
+import Photos
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let photoAccess = PHPhotoLibrary.authorizationStatus()
+        if photoAccess == .authorized {
+            self.launchApp()
+        } else {
+            PHPhotoLibrary.requestAuthorization({ authorization in
+                if authorization == .authorized{
+                    self.launchApp()
+                }else{
+                    let alert = UIAlertController(title: "Access Denied", message: "PhotoTagger requires access to your photo library.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "View settings", style: .default, handler: { (UIAlertAction) in
+                        guard let settingsURL = URL(string: UIApplicationOpenSettingsURLString) else {
+                            return
+                        }
+                        
+                        if UIApplication.shared.canOpenURL(settingsURL) {
+                            UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+                        }
+                    }))
+                    self.window?.rootViewController?.present(alert, animated: false, completion: nil)
+                }
+            })
+        }
         return true
     }
-
+    
+    func launchApp() {
+        //Add launch setup here if necessary
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
--- PhotoTagger/ViewController.swift
+++ PhotoTagger/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    var imageArray = [UIImage]()
+    var myCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myCollectionView = self.view.viewWithTag(2) as! UICollectionView
+        grabPhotos()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) 
+        
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        imageView.image = imageArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let imageController = storyboard?.instantiateViewController(withIdentifier: "ImageViewer")
+        let imageView = imageController?.view.viewWithTag(3) as! UIImageView
+        imageView.image = imageArray[indexPath.row]
+        self.navigationController?.pushViewController(imageController!, animated: true)
+    }
+    
+    func grabPhotos() {
+        
+        DispatchQueue.global(qos: .background).async {
+            print("This is run on the background queue")
+            let imgManager=PHImageManager.default()
+            
+            let requestOptions=PHImageRequestOptions()
+            requestOptions.isSynchronous=true
+            requestOptions.deliveryMode = .highQualityFormat
+            
+            let fetchOptions=PHFetchOptions()
+            fetchOptions.sortDescriptors=[NSSortDescriptor(key:"creationDate", ascending: false)]
+            
+            let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            print(fetchResult)
+            print(fetchResult.count)
+            if fetchResult.count > 0 {
+                for i in 0..<fetchResult.count{
+                    imgManager.requestImage(for: fetchResult.object(at: i) as PHAsset, targetSize: CGSize(width:500, height: 500),contentMode: .aspectFill, options: requestOptions, resultHandler: { (image, error) in
+                        self.imageArray.append(image!)
+                    })
+                }
+            } else {
+                print("You got no photos.")
+            }
+            print("imageArray count: \(self.imageArray.count)")
+            DispatchQueue.main.async {
+                print("This is run on the main queue, after the previous code in outer block")
+                self.myCollectionView.reloadData()
+            }
+        }
+    }
 
 }
 
